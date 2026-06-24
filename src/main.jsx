09784a69b9550fd3e4b88a64796f35c682a778de
azula2580 <azula2580@gmail.com>
2026-06-24 13:03:21 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import axios from 'axios'
 import './App.css'
+
+axios.defaults.baseURL = 'https://exam-backend-hs3y.onrender.com'
+axios.interceptors.request.use(cfg => {
+  const token = localStorage.getItem('token')
+  if (token) cfg.headers.Authorization = `Bearer ${token}`
+  return cfg
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
