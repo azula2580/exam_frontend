c600ad4192a91f5e356480c2ab24aa99bf2b688b
Azula <zulchimegzula@gmail.com>
2026-06-24 11:55:38 +0800

    add axios interceptor

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,12 @@ axios.interceptors.request.use(config => {
   return config
 })
 
+axios.interceptors.request.use(config => {
+  const token = localStorage.getItem('token')
+  if (token) config.headers.Authorization = `Bearer ${token}`
+  return config
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
